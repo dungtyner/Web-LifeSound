2e3aad75a897f3166495a97482381c7b6adbdd9e
manhnhat08 <nhatdang081@gmail.com>
2022-09-25 01:50:33 +0700

    update

--- index.php
+++ index.php
@@ -12,13 +12,14 @@
     <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/celearangce.css">
 
-   
+
     <!-- FONT AWESOME -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
         integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- BOOTSTRAP -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
 </head>
 
@@ -83,7 +84,7 @@
                     <div class="content-sub-content-display">
                         <h2>Basues</h2>
                         <p>This is headphone bluetooth hahahahahahahhahhadhshd</p>
-                        
+
                     </div>
                 </div>
                 <div class="sub-product-display">
@@ -93,7 +94,7 @@
                     <div class="content-sub-content-display">
                         <h2>Basues</h2>
                         <p>This is headphone bluetooth hahahahahahahhahhadhshd</p>
-                        
+
                     </div>
                 </div>
                 <div class="sub-product-display">
@@ -103,7 +104,7 @@
                     <div class="content-sub-content-display">
                         <h2>Basues</h2>
                         <p>This is headphone bluetooth hahahahahahahhahhadhshd</p>
-                        
+
                     </div>
                 </div>
                 <div class="sub-product-display">
@@ -113,10 +114,10 @@
                     <div class="content-sub-content-display">
                         <h2>Basues</h2>
                         <p>This is headphone bluetooth hahahahahahahhahhadhshd</p>
-                        
+
                     </div>
                 </div>
-                
+
 
 
 
@@ -125,7 +126,7 @@
             <div class="close-contetn-data">
                 <i class="fa-solid fa-angles-right"></i>
             </div>
-            
+
         </div>
         <!-- FORM CART  -->
         <div class="cart-content-data">
@@ -157,9 +158,9 @@
                             <div class="one-three-plus-minus-num">
                                 <i class="fa-solid fa-plus"></i>
                             </div>
-                            
-                            
-                            
+
+
+
                         </div>
                     </div>
                     <div class="price-remove-sub-cart-product">
@@ -183,9 +184,9 @@
                             <div class="one-three-plus-minus-num">
                                 <i class="fa-solid fa-plus"></i>
                             </div>
-                            
-                            
-                            
+
+
+
                         </div>
                     </div>
                     <div class="price-remove-sub-cart-product">
@@ -209,9 +210,9 @@
                             <div class="one-three-plus-minus-num">
                                 <i class="fa-solid fa-plus"></i>
                             </div>
-                            
-                            
-                            
+
+
+
                         </div>
                     </div>
                     <div class="price-remove-sub-cart-product">
@@ -235,9 +236,9 @@
                             <div class="one-three-plus-minus-num">
                                 <i class="fa-solid fa-plus"></i>
                             </div>
-                            
-                            
-                            
+
+
+
                         </div>
                     </div>
                     <div class="price-remove-sub-cart-product">
@@ -269,57 +270,165 @@
 
             <div class="header__middle">
                 <ul id="nav">
-                    <li class="nav__item">
-                        <a href="#" class="header__nav__celearance">CELEARANCE</a>
+                    <li class="nav__item ">
+                        <div class="nav__celearance nav-item-menu">
+                            <a href="#" class="header__nav__celearance">CELEARANCE</a>
+                        </div>
                     </li>
                     <li class="nav__item ">
-                        <div class="nav__categories">
+                        <div class="nav__categories nav-item-menu">
                             <a href="#">Categories</a>
                         </div>
                         <ul class="subnav">
-                            <li class="subnav__item"><a href="#">IN-EARS FOR BEGINERS</a></li>
-                            <li class="subnav__item"><a href="#">HEADPHONES FOR BEGINERS</a></li>
-                            <li class="subnav__item"><a href="#">TRUE WIRELESS EARBUDS</a></li>
-                            <li class="subnav__item"><a href="#">WIRELESS HEADPHONES</a></li>
-                            <li class="subnav__item"><a href="#">FLAGSHIP IEMS</a></li>
-                            <li class="subnav__item"><a href="#">FLAGSHIP HEADPHONES</a></li>
+                            <li class="subnav__item">
+                                <a href="#">
+                                    <img src="./images/svg/svgexport-2.svg" alt="">
+                                    <p class="subnav-item-info-img">In-Ears for <br>beginners</p>
+                                </a>
+                            </li>
+                            <li class="subnav__item">
+                                <a href="#">
+                                    <img src="./images/svg/svgexport-3.svg" alt="">
+                                    <p class="subnav-item-info-img">Headphones for <br> beginners</p>
+                                </a>
+                            </li>
+                            <li class="subnav__item">
+                                <a href="#">
+                                    <img src="./images/svg/svgexport-4.svg" alt="">
+                                    <p class="subnav-item-info-img">True Wireless <br> Earbuds</p>
+                                </a>
+                            </li>
+                            <li class="subnav__item">
+                                <a href="#">
+                                    <img src="./images/svg/svgexport-5.svg" alt="">
+                                    <p class="subnav-item-info-img">Wireless <br>Headphones</p>
+                                </a>
+                            </li>
+                            <li class="subnav__item">
+                                <a href="#">
+                                    <img src="./images/svg/svgexport-6.svg" alt="">
+                                    <p class="subnav-item-info-img">Flagship <br>IEMS</p>
+                                </a>
+                            </li>
+                            <li class="subnav__item">
+                                <a href="#">
+                                    <img src="./images/svg/svgexport-7.svg" alt="">
+                                    <p class="subnav-item-info-img">Flagship <br>Headphones</p>
+                                </a>
+                            </li>
+                            <li class="subnav__item">
+                                <a href="#">
+                                    <img src="./images/svg/svgexport-8.svg" alt="">
+                                    <p class="subnav-item-info-img">Hi-Res Audio <br> Players</p>
+                                </a>
+                            </li>
+                            <li class="subnav__item">
+                                <a href="#">
+                                    <img src="./images/svg/svgexport-9.svg" alt="">
+                                    <p class="subnav-item-info-img">Portable Amps <br> &DACs</p>
+                                </a>
+                            </li>
+                            <li class="subnav__item"><a href="#">
+                                    <img src="./images/svg/svgexport-10.svg" alt="">
+                                    <p class="subnav-item-info-img">Desktop Amps & <br> DACs</p>
+                                </a>
+                            </li>
+                            <li class="subnav__item">
+                                <a href="#">
+                                    <img src="./images/svg/svgexport-11.svg" alt="">
+                                    <p class="subnav-item-info-img">Work From <br> Home</p>
+                                </a>
+                            </li>
+                            <li class="subnav__item">
+                                <a href="#">
+                                    <img src="./images/svg/svgexport-12.svg" alt="">
+                                    <p class="subnav-item-info-img">Gaming</p>
+                                </a>
+                            </li>
+                            <li class="subnav__item">
+                                <a href="#">
+                                    <img src="./images/svg/svgexport-13.svg" alt="">
+                                    <p class="subnav-item-info-img">Studio & <br> Professional</p>
+                                </a>
+                            </li>
+                            <li class="subnav__item">
+                                <a href="#">
+                                    <img src="./images/svg/svgexport-14.svg" alt="">
+                                    <p class="subnav-item-info-img">Speakers</p>
+                                </a>
+                            </li>
+                            <li class="subnav__item">
+                                <a href="#">
+                                    <img src="./images/svg/svgexport-15.svg" alt="">
+                                    <p class="subnav-item-info-img">Cables</p>
+                                </a>
+                            </li>
+                            <li class="subnav__item">
+                                <a href="#">
+                                    <img src="./images/svg/svgexport-16.svg" alt="">
+                                    <p class="subnav-item-info-img">Eartips & <br> Earpads</p>
+                                </a>
+                            </li>
+                            <li class="subnav__item">
+                                <a href="#">
+                                    <img src="./images/svg/svgexport-17.svg" alt="">
+                                    <p class="subnav-item-info-img""> Cases</p></a>
+                            </li>
                         </ul>
                     </li>
-                    <li class="nav__item">
-                        <div class="nav__brands">
-                            <a href="#">Brands</a>
-                        </div>
-                        <ul class="subnav">
-                            <li class="subnav__item"><a href="#">1Custom</a></li>
-                            <li class="subnav__item"><a href="#">64 Audio</a></li>
-                            <li class="subnav__item"><a href="#">Abyss</a></li>
-                            <li class="subnav__item"><a href="#">AlAlAl</a></li>
-                            <li class="subnav__item"><a href="#">AKG</a></li>
-                            <li class="subnav__item"><a href="#">ALO Audio</a></li>
-                            <li class="subnav__item"><a href="#">Altiat</a></li>
-                            <li class="subnav__item"><a href="#">Astell&Kern</a></li>
-                            <li class="subnav__item"><a href="#">Audeze</a></li>
-                            <li class="subnav__item"><a href="#">AudioQuest</a></li>
-                            <li class="subnav__item"><a href="#">Aune Audio</a></li>
-                            <li class="subnav__item"><a href="#">Austrian Audio</a></li>
-                            <!-- <li class="subnav__item"><a href="#">AZLA</a></li>
-                            <li class="subnav__item"><a href="#">Bang & Olufsen</a></li>
-                            <li class="subnav__item"><a href="#">Beyerdynamic</a></li> -->
+                    <li class=" nav__item">
+                                        <div class="nav__brands nav-item-menu">
+                                            <a href="#">Brands</a>
+                                        </div>
+                                        <ul class="subnav  brands-list-item">
+                                            <li class="subnav__item"><a href="#">1Custom</a></li>
+                                            <li class="subnav__item"><a href="#">64 Audio</a></li>
+                                            <li class="subnav__item"><a href="#">Abyss</a></li>
+                                            <li class="subnav__item"><a href="#">AlAlAl</a></li>
+                                            <li class="subnav__item"><a href="#">AKG</a></li>
+                                            <li class="subnav__item"><a href="#">ALO Audio</a></li>
+                                            <li class="subnav__item"><a href="#">Altiat</a></li>
+                                            <li class="subnav__item"><a href="#">Astell&Kern</a></li>
+                                            <li class="subnav__item"><a href="#">Audeze</a></li>
+                                            <li class="subnav__item"><a href="#">AudioQuest</a></li>
+                                            <li class="subnav__item"><a href="#">Aune Audio</a></li>
+                                            <li class="subnav__item"><a href="#">Austrian Audio</a></li>
+                                            <li class="subnav__item"><a href="#">AZLA</a></li>
+                                            <li class="subnav__item"><a href="#">Bang & Olufsen</a></li>
+                                            <li class="subnav__item"><a href="#">BLON</a></li>
+                                            <li class="subnav__item"><a href="#">Bowers & Wilkins</a></li>
+                                            <li class="subnav__item"><a href="#">Burson Audio</a></li>
+                                            <li class="subnav__item"><a href="#">Campfire Audio</a></li>
+                                            <li class="subnav__item"><a href="#">Cayin</a></li>
+                                            <li class="subnav__item"><a href="#">CCA</a></li>
+                                            <li class="subnav__item"><a href="#">Chord Electronics</a></li>
+                                            <li class="subnav__item"><a href="#">Comply</a></li>
+                                            <li class="subnav__item"><a href="#">Dali</a></li>
+                                            <li class="subnav__item"><a href="#">Dan Clark Audio</a></li>
+                                            <li class="subnav__item"><a href="#">DarkVoice</a></li>
+                                            <li class="subnav__item"><a href="#">ddHiFi</a></li>
+                                            <li class="subnav__item"><a href="#">Dekoni Audio </a></li>
+                                            <li class="subnav__item"><a href="#">Denafrips</a></li>
+                                            <li class="subnav__item"><a href="#">Devialet</a></li>
+                                            <li class="subnav__item"><a href="#">EarMen</a></li>
+                                            <li class="subnav__item"><a href="#">Effect Audio</a></li>
+                                            <li class="subnav__item"><a href="#">EIKON</a></li>
+                                            <li class="subnav__item"><a href="#">EPOS</a></li>
+                                        </ul>
+                            </li>
+                            <li class="nav__item nav-item-menu">
+                                <a href="#">Price</a>
+                            </li>
+                            <li class="nav__item nav-item-menu">
+                                <a href="#">Deals</a>
+                            </li>
+                            <li class="nav__item nav-item-menu">
+                                <a href="#">Forum</a>
+                            </li>
+                            <li class="nav__item nav-item-menu">
+                                <a href="views/developer.html">Developer</a>
+                            </li>
                         </ul>
-                    </li>
-                    <li class="nav__item">
-                        <a href="#">Price</a>
-                    </li>
-                    <li class="nav__item">
-                        <a href="#">Deals</a>
-                    </li>
-                    <li class="nav__item">
-                        <a href="#">Forum</a>
-                    </li>
-                    <li class="nav__item">
-                        <a href="#">Developer</a>
-                    </li>
-                </ul>
 
 
 
@@ -329,7 +438,7 @@
                 <ul class="header__linklist">
                     <a href="#" class="click-form-search">Search</a>
                 </ul>
-                <ul class="header__linklist">  
+                <ul class="header__linklist">
                     <a href="views/login.php">Account</a>
                     <!-- <li class="menu_login"><a href="views/login.php">Đăng nhập</a></li> -->
                     <!-- <li class="menu_login"><a href="#">Đăng ký</a></li>
@@ -337,20 +446,20 @@
                 </ul>
                 <ul class="header__linklist">
                     <a href="#" class="click-form-cart">Cart
-                    <span class="header__cart__count">0</span>
-                </a>
+                        <span class="header__cart__count">0</span>
+                    </a>
 
                 </ul>
             </div>
 
-        <!-- Header end -->
+            <!-- Header end -->
 
 
-        
 
 
-    </div>
-<!-- CONTENT -->
+
+        </div>
+        <!-- CONTENT -->
         <div class="content-life-sound">
             <div class="left-content-title">
                 <h1>Hello, We are 3 Anh, <br>Welcome to Live Sound Shop !</h1>
@@ -359,7 +468,7 @@
                         <i class="fa-solid fa-check"></i>
                         <p>Singin Now !</p>
                     </div>
-                    
+
                     <div class="row-three-subtitle-nice-home">
                         <i class="fa-solid fa-check"></i>
                         <p>Singin Now !</p>
@@ -377,7 +486,7 @@
                 </div>
             </div>
             <div class="model-3d-content">
-                <model-viewer class="threeD-edit-home-model" src="images/3D_Product/Phone1/scene.gltf" alt="VR Headset" auto-rotate camera-controls autoplay ar ios-src="Picture_Dinosaur/T-Rex/scene.gltf"></model-viewer>
+                <!-- <model-viewer class="threeD-edit-home-model" src="images/3D_Product/Phone1/scene.gltf" alt="VR Headset" auto-rotate camera-controls autoplay ar ios-src="Picture_Dinosaur/T-Rex/scene.gltf"></model-viewer> -->
                 <!-- <i class="fa-solid fa-circle-xmark" onclick="close_display()"></i> -->
             </div>
         </div>
